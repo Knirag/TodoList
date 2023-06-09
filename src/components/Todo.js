b4837d28a099bc5f6eaac0ffdd76e626428881cb
Knirag <105249761+Knirag@users.noreply.github.com>
2023-06-09 21:19:14 +0200

    Comments Part 1

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,21 +1,35 @@
-import React, { useState } from 'react';
-import TodoForm from './TodoForm';
-import { RiCloseCircleLine } from 'react-icons/ri';
-import { TiEdit } from 'react-icons/ti';
+import React, { useState } from 'react'; //'react' module
+import TodoForm from './TodoForm';// Imports the TodoForm component from the TodoForm file
+import { RiCloseCircleLine } from 'react-icons/ri'; // Import the close/delete icon from the React Icons Library
+import { TiEdit } from 'react-icons/ti'; // Import the Eddit/Modify Icon from the React Icon Library
 
+const initialState = {
+  id: null,
+  value:''
+  //Defining constant variable initialState with its 2 properties id & value, id  stores the identifier and value to store any data
+}
 function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
-  const [edit, setEdit] = useState({
-    id: null,
-    value: ''
-  });
-
+  //Todo function that contains the todos,completeTodo,removeTodo and updateTodo parameters
+  //Willl show the list of todos and enable deletion,addition or editing
+  const [edit, setEdit] = useState(initialState);
+  // use of'useState' to initialize the edit state variable with that found in the initial state variable and setEdit function to update the edit variable.
+ const myState = useState(initialState);
+ //use of useState to initialize the myState variable with the initial state value and 
+ console.log(myState);
+ //logs the value of myState to the console
+ const edit2 = myState[0];
+ //assigning the first array element of mySate to the variable edit2
+ const setEdit2 = myState[1];
+ //assigning the second array element of mySate to the variable setEdit2
   const submitUpdate = value => {
-    // Call the updateTodo function passed from the parent component to update the todo with the new value
+   // variable declaration of submitUpdate function
     updateTodo(edit.id, value);
+    //Updates a specific todo item with a new value using the updateTodo function
     setEdit({
       id: null,
       value: ''
     });
+    //Resets the state of the edit variable
   };
 
   if (edit.id) {
@@ -26,6 +40,7 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
   return (
     <>
       {todos.map((todo, index) => (
+        //Assigns each todo item to the todo variable and the index to the index variable
         <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
           <div key={todo.id} onClick={() => completeTodo(todo.id)}>
             {todo.text}
@@ -45,7 +60,6 @@ function Todo({ todos, completeTodo, removeTodo, updateTodo }) {
         </div>
       ))}
       {/* Render the TodoForm component for adding new todos */}
-      <TodoForm />
     </>
   );
 }
